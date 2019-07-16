9988cd96486f01263479ff9327db4032750a1dd6
pellst <pellst@shaw.ca>
2019-07-16 06:00:19 -0600

    incl additional scripts to run on ARC
    Added cores and replicates params to ConfigImp.yaml
    array_job.sh is a slurm script to run n jobs
    mjob_run.sh is a script to clone the imp_model folder for the job run number

---
+++ mss_out/imp_model/mjob_run.sh
@@ -0,0 +1,28 @@
+# !/bin/bash -x
+
+# usage: mjob_run.sh 6
+# first param is the replicate number
+num_repl=$1
+repl_name=imp_model_$1
+echo "replicates number:"$repl_name
+#set defaults for cores and replicates
+export cores="1"
+export replicates="1"
+#cd ../
+#cp -R imp_model imp_model_2
+#cp -R imp_model imp_model_3
+#sed -e 's/:[^:\/\/]/="/g;s/$/"/g;s/ *=/=/g' ConfigImp.yaml > param.sh
+cat ConfigImp.yaml | grep -e cores -e replicates | sed -e 's/:[^:\/\/]/="/g;s/$/"/g;s/ *=/=/g' | sed -e 's/ //g' | sed -e 's/^/export /g' > param.sh
+chmod 755 param.sh
+#source the env vars from param.sh as these are exported
+. param.sh
+echo "cores:"$cores
+echo "replicates:"$replicates 
+# from imp_model folder we clone the driver scripts
+cd ../
+cp -R imp_model $repl_name
+cd $repl_name
+sbatch run_imp.slurm
+squeue -u troy.pells
+
+
