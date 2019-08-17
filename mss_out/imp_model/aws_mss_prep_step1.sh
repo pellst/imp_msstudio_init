5271a6635123d748472fe6ac7af0cdd871874542
pellst <pellst@shaw.ca>
2019-08-17 16:57:21 -0600

    Update aws_mss_prep_step1.sh

--- mss_out/imp_model/aws_mss_prep_step1.sh
+++ mss_out/imp_model/aws_mss_prep_step1.sh
@@ -11,4 +11,5 @@ unzip master.zip
 #cd /scratch/imp/imp_msstudio_init-master/driver_imp_mss/imp_model
 cd /tmp/imp/imp_msstudio_init-master/mss_out/imp_model
 cd /tmp/imp/imp_msstudio_init-master/mss_out/imp_model
-chmod 755 /tmp/imp/imp_msstudio_init-master/mss_out/imp_model/aws_mss_prep_step*
+#chmod 755 /tmp/imp/imp_msstudio_init-master/mss_out/imp_model/aws_mss_prep_step*
+chmod 777 /tmp/imp/imp_msstudio_init-master/mss_out/*
