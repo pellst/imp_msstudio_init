52df4b0dc7f281c81f746590cf62dc2e07e4c5d4
pellst <pellst@shaw.ca>
2019-08-19 16:11:44 -0600

    remove sudo for compute canada

--- mss_out/imp_model/uoc_mss_prep_step2.sh
+++ mss_out/imp_model/uoc_mss_prep_step2.sh
@@ -3,8 +3,8 @@ cd /home/tpells/scratch
 #From the scratch folder on Cedar:
 # get latest anaconda and then run install step
 curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
-sudo bash Anaconda3-2019.03-Linux-x86_64.sh -b -p /home/tpells/scratch/anaconda 
-sudo rm -rf /home/tpells/scratch/Anaconda3-2019.03-Linux-x86_64.sh
+bash Anaconda3-2019.03-Linux-x86_64.sh -b -p /home/tpells/scratch/anaconda 
+rm -rf /home/tpells/scratch/Anaconda3-2019.03-Linux-x86_64.sh
 export PYTHONPATH="/home/tpells/scratch/anaconda"
 export PATH="/home/tpells/scratch/anaconda/bin:$PATH"
 conda init bash
