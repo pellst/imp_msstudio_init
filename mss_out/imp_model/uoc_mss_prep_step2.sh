5c3c07e773fb1ec812e73577e7dc52b761caa9a5
pellst <pellst@shaw.ca>
2019-09-10 18:21:20 -0600

    Update uoc_mss_prep_step2.sh

--- mss_out/imp_model/uoc_mss_prep_step2.sh
+++ mss_out/imp_model/uoc_mss_prep_step2.sh
@@ -4,8 +4,10 @@ cd /home/$USER/scratch
 # get latest anaconda and then run install step
 #curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 curl -O https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
-bash Anaconda3-2019.03-Linux-x86_64.sh -b -p /home/$USER/scratch/anaconda 
-rm -rf /home/$USER/scratch/Anaconda3-2019.03-Linux-x86_64.sh
+bash Anaconda3-2019.07-Linux-x86_64.sh -b -p /home/$USER/scratch/anaconda 
+#bash Anaconda3-2019.03-Linux-x86_64.sh -b -p /home/$USER/scratch/anaconda 
+rm -rf /home/$USER/scratch/Anaconda3-2019.07-Linux-x86_64.sh
+#rm -rf /home/$USER/scratch/Anaconda3-2019.03-Linux-x86_64.sh
 export PYTHONPATH="/home/$USER/scratch/anaconda"
 export PATH="/home/$USER/scratch/anaconda/bin:$PATH"
 conda init bash
