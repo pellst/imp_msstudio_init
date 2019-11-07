0b24ffe06bf3c88a787067a323ce5dca0f1c5d9a
pellst <pellst@shaw.ca>
2019-11-07 07:40:53 -0700

    Create aws_run_onenode.sh

---
+++ mss_out/imp_model/aws_run_onenode.sh
@@ -0,0 +1,12 @@
+#/usr/bin/bash -x
+runnum=$1
+# pip install awscli --upgrade --user
+
+sudo mkdir /shared/imp/imp_msstudio_init-master/mss_out/imp_model$runnum
+cd      /shared/imp/imp_msstudio_init-master/mss_out/imp_model$runnum
+sudo cp /shared/imp/imp_msstudio_init-master/mss_out/imp_model/*.* /shared/imp/imp_msstudio_init-master/mss_out/imp_model$runnum
+nohup /shared/anaconda/bin/python prep_hyperp_imp_v2ux.py --count=1 --name=DemoImpModel --config=ConfigImp.yaml >/shared/imp/imp_msstudio_init-master/mss_out/imp_model$runnum/trace.log 2>&1 &
+
+# archive
+# tar -cvzf imp_model18.tgz /shared/imp/imp_msstudio_init-master/mss_out/imp_model18
+# aws s3 cp imp_model18.tgz s3://pcluster-resource
