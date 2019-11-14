818ac4f0ee54fde6d84fd487049f4f195d41d3c5
pellst <pellst@shaw.ca>
2019-11-14 01:10:46 -0700

    amend conda version and imp pkg install
    revert to previous conda version and setup of imp pkg required ( two steps )

--- mss_out/imp_model/uoc_mss_prep_step2.sh
+++ mss_out/imp_model/uoc_mss_prep_step2.sh
@@ -2,12 +2,13 @@
 cd /home/$USER/scratch
 #From the scratch folder on Cedar:
 # get latest anaconda and then run install step
-#curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
-curl -O https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
-bash Anaconda3-2019.07-Linux-x86_64.sh -b -p /home/$USER/scratch/anaconda 
-#bash Anaconda3-2019.03-Linux-x86_64.sh -b -p /home/$USER/scratch/anaconda 
-rm -rf /home/$USER/scratch/Anaconda3-2019.07-Linux-x86_64.sh
-#rm -rf /home/$USER/scratch/Anaconda3-2019.03-Linux-x86_64.sh
+curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
+bash Anaconda3-2019.03-Linux-x86_64.sh -b -p /home/$USER/scratch/anaconda 
+rm -rf /home/$USER/scratch/Anaconda3-2019.03-Linux-x86_64.sh
+# later version has issue running on cedar and graham - index out of range
+#curl -O https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
+#bash Anaconda3-2019.07-Linux-x86_64.sh -b -p /home/$USER/scratch/anaconda 
+#rm -rf /home/$USER/scratch/Anaconda3-2019.07-Linux-x86_64.sh
 export PYTHONPATH="/home/$USER/scratch/anaconda"
 export PATH="/home/$USER/scratch/anaconda/bin:$PATH"
 conda init bash
--- mss_out/imp_model/uoc_mss_prep_step3.sh
+++ mss_out/imp_model/uoc_mss_prep_step3.sh
@@ -8,6 +8,8 @@ conda activate
 conda --version
 # then we can get imp setup
 conda config --add channels salilab
-conda install -y imp numpy scipy scikit-learn matplotlib
-#conda install -y numpy scipy 
+conda install -y imp scikit-learn matplotlib
+conda install -y numpy scipy 
+# avoid this next version with all in one step as it is suspect
+#conda install -y imp numpy scipy scikit-learn matplotlib
 #scikit-learn matplotlib
