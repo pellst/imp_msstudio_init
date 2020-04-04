4172c14432c048be835486050c2a8525cefa5c93
pellst <pellst@shaw.ca>
2020-04-04 16:50:59 -0600

    Update uoc_mss_prep_step3.sh
    take latest release of  IMP instead of specifying ver 2.11.1

--- mss_out/imp_model/uoc_mss_prep_step3.sh
+++ mss_out/imp_model/uoc_mss_prep_step3.sh
@@ -8,8 +8,8 @@ conda activate
 conda --version
 # then we can get imp setup
 conda config --add channels salilab
-#conda install -y imp 
-conda install -c salilab imp=2.11.1=py37hf484d3e_1
+conda install -y imp 
+#conda install -c salilab imp=2.11.1=py37hf484d3e_1
 conda install -y scikit-learn matplotlib
 conda install -y numpy scipy 
 # avoid this next version with all in one step as it is suspect
