95a91cbe3fedf6c033d70acaf3862a60eab6ae6f
pellst <pellst@shaw.ca>
2020-05-07 13:10:09 -0600

    Update mjob_run_cedar.sh
    add replicate_job_num and loop through to clone that number of job runs

--- mss_out/imp_model/mjob_run_cedar.sh
+++ mss_out/imp_model/mjob_run_cedar.sh
@@ -4,6 +4,7 @@
 # first param is the replicate number
 num_repl=$1
 repl_name=imp_model_$1
+#repl_job_count=2
 echo "replicates number:"$repl_name
 #set defaults for cores and replicates
 export cores="1"
@@ -15,7 +16,12 @@ cat ConfigImp.yaml | grep -e cores | sed -e 's/:[^:\/\/]/="/g;s/$/"/g;s/ *=/=/g'
 chmod 755 param.sh
 #source the env vars from param.sh as these are exported
 . param.sh
+cat ConfigImp.yaml | grep -e replicates | sed -e 's/:[^:\/\/]/="/g;s/$/"/g;s/ *=/=/g' | sed -e 's/ //g' | sed -e 's/^/export /g' > param.sh
+chmod 755 param.sh
+#source the env vars from param.sh as these are exported
+. param.sh
 echo "cores:"$cores
+echo "replicate job count:" $replicates
 # construct slurm file
 cat << EOF > param_prep.slurm
 #!/bin/bash
@@ -25,8 +31,8 @@ cat << EOF > param_prep.slurm
 #SBATCH --ntasks-per-node=16
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32G
-#SBATCH --time=0-10:00:00
-#SBATCH --account=sponsor
+#SBATCH --time=0-01:00:00
+#SBATCH --account=def-dschriem
 
 
 mpiexec -n $cores python prep_hyperp_imp_v2ux.py --count=1 --name=DemoImpModel --config=ConfigImp.yaml
@@ -40,10 +46,25 @@ sed -e "s/\r//" param_prep.slurm > run_imp.slurm
 rm -rf param_prep.slurm
 rm -rf param.sh
 # from imp_model folder we clone the driver scripts
-cd ../
-cp -R imp_model $repl_name
-cd $repl_name
-sbatch run_imp.slurm
-squeue -u tpells
+# replicates job count repeat clone incrementing repl_name by 1 each time
+#repl_job_count=2
+START=1
+END=`echo -n $replicates | tr -d '\r' `
+i=$START
+for (( repljob=$START; repljob<=$END; repljob++))
+#while [[ $i -le $END ]]
+do
+    #echo -n "$i "
+    #((i = i+1  ))
+    #sleep 1
+    echo $repl_name$repljob
+
+    #cd ../
+    cp -R ../imp_model ../$repl_name$repljob
+    #cd $repl_name$repljob
+    #sbatch run_imp.slurm
+    #squeue -u tpells
+    #cd ../
+done
 
 
