0cb1c089d2e7dfacf7b73c15f555562b69c216cd
pellst <pellst@shaw.ca>
2020-08-25 11:45:02 -0600

    Update aws_mss_prep_step3.sh
    pull latest imp version

--- mss_out/imp_model/aws_mss_prep_step3.sh
+++ mss_out/imp_model/aws_mss_prep_step3.sh
@@ -9,7 +9,8 @@ conda --version
 # then we can get imp setup
 conda config --add channels salilab
 #conda install -y imp scikit-learn matplotlib
-conda install -c salilab imp=2.11.1=py37hf484d3e_1
+#conda install -c salilab imp=2.11.1=py37hf484d3e_1
+conda install -y imp
 conda install -y scikit-learn matplotlib
 conda install -y numpy scipy 
 #scikit-learn matplotlib
