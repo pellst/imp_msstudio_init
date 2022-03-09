0ee33cf1f3a202b010a555fa77c9d996415d5f22
pellst <pellst@shaw.ca>
2022-03-09 10:36:34 -0700

    amended scripts
    setup param for XLRestraint to pass database= in place of CrossLinkDataBase=
    aws_run_onenode.sh version for running on imp compliled from source
    ConfigImp.yaml has emdb section added

--- mss_out/imp_model/aws_run_onenode.sh
+++ mss_out/imp_model/aws_run_onenode.sh
@@ -1,61 +1,21 @@
 #/usr/bin/bash -x
-cd "$(dirname ${BASH_SOURCE[0]})"
 runnum=$1
 # pip install awscli --upgrade --user
 
-# first param is the replicate number
-num_repl=$1
-repl_name=imp_model_$1
-#repl_job_count=2
-echo "replicates number:"$repl_name
-#set defaults for cores and replicates
-export cores="1"
-#cd ../
-#cp -R imp_model imp_model_2
-#cp -R imp_model imp_model_3
-#sed -e 's/:[^:\/\/]/="/g;s/$/"/g;s/ *=/=/g' ConfigImp.yaml > param.sh
-cat ConfigImp.yaml | grep -e cores | sed -e 's/:[^:\/\/]/="/g;s/$/"/g;s/ *=/=/g' | sed -e 's/ //g' | sed -e 's/^/export /g' > param.sh
-chmod 755 param.sh
-#source the env vars from param.sh as these are exported
-. param.sh
-cat ConfigImp.yaml | grep -e replicates | sed -e 's/:[^:\/\/]/="/g;s/$/"/g;s/ *=/=/g' | sed -e 's/ //g' | sed -e 's/^/export /g' > param.sh
-chmod 755 param.sh
-#source the env vars from param.sh as these are exported
-. param.sh
-echo "cores:"$cores
-echo "replicate job count:" $replicates
 
+sudo mkdir /shared/imp/imp_msstudio_init-master/mss_out/imp_model$runnum
+sudo chmod 777 /shared/imp/imp_msstudio_init-master/mss_out/imp_model$runnum
+cd      /shared/imp/imp_msstudio_init-master/mss_out/imp_model$runnum
+sudo cp /shared/imp/imp_msstudio_init-master/mss_out/imp_model/*.* /shared/imp/imp_msstudio_init-master/mss_out/imp_model$runnum
 
+#sudo /shared/anaconda/bin/mpiexec -n 8 /shared/anaconda/bin/python prep_hyperp_imp_v2_14_0ux.py --count=1 --name=DemoImpModel --config=ConfigImp.yaml >/shared/imp/imp_msstudio_init-master/mss_out/imp_model$runnum/trace.log 2>&1
+#sudo /shared/anaconda/bin/mpiexec -n 8 /shared/anaconda/bin/python prep_hyperp_imp_v2_14_0ux.py --count=1 --name=DemoImpModel --config=ConfigImp.yaml --testing --skipmcrun >/shared/imp/imp_msstudio_init-master/mss_out/imp_model$runnum/trace.log 2>&1
 
-#repl_job_count=2
-START=1
-END=`echo -n $replicates | tr -d '\r' `
-i=$START
-for (( repljob=$START; repljob<=$END; repljob++))
-#while [[ $i -le $END ]]
-do
-    #echo -n "$i "
-    #((i = i+1  ))
-    #sleep 1
-    echo $repl_name$repljob
-    
-	sudo cp -R ../imp_model ../$repl_name$repljob
-    cd ../$repl_name$repljob
-	
+#sudo /home/ec2-user/imp214/imp_release/setup_environment.sh /shared/anaconda/bin/mpiexec -n 4 /shared/anaconda/bin/python prep_hyperp_imp_v2_14_0ux.py --count=1 --name=DemoImpModel --config=ConfigImp.yaml --testing >/shared/imp/imp_msstudio_init-master/mss_out/imp_model$runnum/trace.log 2>&1
 
-	# number of cores on instance determines param for -N 16
-	#nohup /shared/anaconda/bin/mpiexec -N $cores /shared/anaconda/bin/python prep_hyperp_imp_v2ux.py --count=1 --name=DemoImpModel --config=ConfigImp.yaml >/shared/imp/danhells/imp_model$runnum/trace.log 2>&1 &
-	# we can only run this in series and not in parallel on aws. Additional instances need to be launched to run replicate job runs in parallel
-	/shared/anaconda/bin/mpiexec -N $cores /shared/anaconda/bin/python prep_hyperp_imp_v2ux.py --count=1 --name=DemoImpModel --config=ConfigImp.yaml > trace.log 2>&1 &
-
-    cd ../imp_model
-
-done
-
-
-
+sudo /home/ec2-user/imp214/imp_release/setup_environment.sh /shared/anaconda/bin/python prep_hyperp_imp_v2_14_0ux_v2a.py --count=1 --name=DemoImpModel --config=ConfigImp.yaml --testing >/shared/imp/imp_msstudio_init-master/mss_out/imp_model$runnum/trace.log 2>&1
 
 
 # archive
-# tar -cvzf imp_model18.tgz /shared/imp/danhells/imp_model18
+# tar -cvzf imp_model18.tgz /shared/imp/imp_msstudio_init-master/mss_out/imp_model18
 # aws s3 cp imp_model18.tgz s3://pcluster-resource
